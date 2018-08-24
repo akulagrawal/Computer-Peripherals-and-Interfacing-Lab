95fa3108538552c88ee8f18fc45dee60fcba91e5
Akul Agrawal <akulagrawal@rediffmail.com>
2018-08-25 00:30:24 +0530

    Final changes

--- Assignment01/16BitCalculator.asm
+++ Assignment01/16BitCalculator.asm
@@ -24,32 +24,31 @@ STA 8008H
 STA 8009H
 
 ; ------------- Check the operation -----------------
-LDA 8001H
 CALL COPYIN
 
+LDA 8001H
 CPI 01H
 CZ ADD
-CALL COPYOUT
+LDA 8001H
+CPI 01H
+CZ COPYOUT
 
 LDA 8001H
 CPI 02H
 CZ SUB
-CALL COPYOUT
+LDA 8001H
+CPI 02H
+CZ COPYOUT
 
 LDA 8001H
 CPI 03H
 CZ MUL
-CALL COPYOUT
 
 LDA 8001H
 CPI 04H
 CZ DIV
 
 RST 5
-
-; ---- If invalid operation, default to addition ----
-JMP ADD
-
 
 ; ------------- Add -----------------
 ADD:
@@ -94,97 +93,138 @@ RET
 ; ------------- Mul -----------------
 MUL:
 
-LDA 8013H
-MOV B, A
-MVI D, 00H
-MVI A, 00H
-LDA 8015H
-MOV E, A
-MVI A, 00H
-CALL MUL8B
+; --- STORE E ---
+LDA 8002H
+STA 800AH
+LDA 8003H
+STA 800BH
 
-; -------- Store last 16 bits (Y1Y2) -------
-LDA 800BH
-STA 8019H
-LDA 800AH
-STA 8018H
-
-; ------- 
-LDA 8012H
-MOV B, A
-MVI D, 00H
+;--------- Start ------
 MVI A, 00H
-LDA 8014H
-MOV E, A
-MVI A, 00H
-CALL MUL8B
-
-; ----- Store first 16 bits (X1X2) -------
-LDA 800BH
-STA 8017H
-LDA 800AH
+STA 8006H
+STA 8007H
+STA 8008H
+STA 8009H
 STA 8016H
+STA 8017H
+STA 8018H
+STA 8019H
 
-; ------
-LDA 8012H
-MOV B, A
-MVI D, 00H
-MVI A, 00H
-LDA 8015H
-MOV E, A
-MVI A, 00H
-CALL MUL8B
+LOOP:
+    ;---- COPY B ---  
+    LDA 8004H
+    STA 8014H
+    LDA 8005H
+    STA 8015H
 
+    ;---- COPY TEMP SUM ---
+    LDA 8008H
+    STA 8012H
+    LDA 8009H
+    STA 8013H
 
-; ----- 8 bit multiplication subroutine ----
-; ----- stores values in 800AH and 800BH ---
-MUL8B:
-LOOP: 
-      ADD B
-	    JNC NEXT
-      INR D
-NEXT: 
-      DCR E
-      JNZ LOOP
-      STA 800BH
-	    MOV A, D
-	    STA 800AH
+    ; ---- ADD B + TEMP SUM ---
+    CALL ADD
+
+    ; --- WRITE BACK TEMP SUM ---
+    LDA 8018H
+    STA 8008H
+    LDA 8019H
+    STA 8009H
+    
+    ; ---- CHECK CARRY ----   
+    LDA 8017H
+    CPI 00H
+
+    ; ---- JUMP ON NO CARRY ---
+    JZ NEXT
+
+    ; ---- IF CARRY, INCREMENT 8006-07 ---
+    LDA 8006H
+    STA 8012H
+    LDA 8007H
+    STA 8013H
+    MVI A, 00H
+    STA 8014H
+    MVI A, 01H
+    STA 8015H
+    CALL ADD 
+
+    ; --- WRITE BACK ----
+    LDA 8018H
+    STA 8006H
+    LDA 8019H
+    STA 8007H   
+
+    MVI C, 11H
+    
+NEXT:
+    ; --- DECREMENT E ----
+    LDA 800AH
+    STA 8012H
+    LDA 800BH
+    STA 8013H
+    MVI A, 00H
+    STA 8014H
+    MVI A, 01H
+    STA 8015H
+
+    CALL SUB
+
+    ; ---- STORE DECREMENTED RESULT ---
+    LDA 8018H
+    STA 800AH
+    LDA 8019H
+    STA 800BH
+
+    ; ---- CHECK IF ZERO ---
+    LDA 800AH
+    MOV B, A
+    LDA 800BH
+    ORA B
+    CPI 00H
+    JNZ LOOP
 
 RET
 
 ; ------------- Div -----------------
 DIV:
 
- LHLD 8002H                ; Get the dividend
-       LDA 8005H                ; Get the divisor
-       MOV C, A
-       LXI D, 0000H                ; Quotient = 0
-BACK: MOV A, H
-       SUB C                        ; Subtract divisor
-       MOV H, A                ; Save partial result
-       JNC SKIP                ; if CY  1 jump
-       DCR L                ; Subtract borrow of previous subtraction
-SKIP: INX D                        ; Increment quotient
-       MOV A, L
-       CPI 00                ; Check if dividend < divisor
-       JNZ BACK                ; if no repeat
-AGAIN: MOV A, H
-       SUB C
-       JC END
-       INX D
-       MOV H, A
-       JMP AGAIN
-END:   SHLD 8008H                ; Store the remainder
-       XCHG
-       MOV A, H
-      STA  8006H
-      MOV A, L
-      STA 8007H
-                     ; Store the quotient
-RET                        ; Terminate program execution
+LHLD 8002H                      ; Get the dividend
+LDA 8005H                       ; Get the divisor
+MOV C, A
+LXI D, 0000H                    ; Quotient = 0
+
+BACK:   MOV A, H
+        SUB C                   ; Subtract divisor
+        MOV H, A                ; Save partial result
+        JNC SKIP                ; if CY  1 jump
+        DCR L                   ; Subtract borrow of previous subtraction
+
+SKIP:   INX D                   ; Increment quotient
+        MOV A, L
+        CPI 00                  ; Check if dividend < divisor
+        JNZ BACK                ; if no repeat
+
+AGAIN:  MOV A, H
+        SUB C
+        JC END
+        INX D
+        MOV H, A
+        JMP AGAIN
+
+END:    SHLD 8008H              ; Store the remainder
+        XCHG
+        MOV A, H
+        STA  8006H
+        MOV A, L
+        STA 8007H               ; Store the quotient
+                                
+RET                             ; Return back
 
 ; ------------- Copy Input ------------
 COPYIN:
+
 LDA 8002H
 STA 8012H
 LDA 8003H
@@ -197,6 +237,7 @@ RET
 
 ; ------------- Copy Output -----------
 COPYOUT:
+
 LDA 8016H
 STA 8006H
 LDA 8017H
--- Assignment01/8BitCalculator.asm
+++ Assignment01/8BitCalculator.asm
@@ -5,247 +5,108 @@ ORG 9000H
 
 ; ----------------------------------------------------
 ; 8001 - Operation
-; 8002-03  -  Input 1
-; 8004-05  -  Input 2
-; 8006-09  -  Output 
-; 800A-0B  -  Temporary Storage for MUL
-; 8012-13  -  Temporary Input 1
-; 8014-15  -  Temporary Input 2
-; 8016-19  -  Temporary Output
+; 8002 - Input 1
+; 8003 -  Input 2
+; 8004-05  - Output 
 ; ----------------------------------------------------
 
 ; ------------- Initialize carry to zero -------------
 INIT:
 MVI C, 00H
 MOV A, C
-STA 8006H
-STA 8007H
-STA 8008H
-STA 8009H
+STA 8005H
+
+; ------------- Load B from memory location -----------------
+LDA 8002H
+MOV B, A
 
 ; ------------- Check the operation -----------------
-CALL COPYIN
-
 LDA 8001H
+
 CPI 01H
-CZ ADD
-LDA 8001H
-CPI 01H
-CZ COPYOUT
+JZ ADD
 
-LDA 8001H
 CPI 02H
-CZ SUB
-LDA 8001H
-CPI 02H
-CZ COPYOUT
+JZ SUB
 
-LDA 8001H
 CPI 03H
-CZ MUL
+JZ MUL
 
-LDA 8001H
 CPI 04H
-CZ DIV
+JZ DIV
 
-RST 5
+; -------- If invalid operation, default to addition ---------
+JNZ ADD
 
 ; ------------- Add -----------------
 ADD:
 
-MVI A, 00H
-STA 8016H
-LDA 8013H
-MOV B, A
-LDA 8015H
+LDA 8003H
 ADD B
-STA 8019H
-LDA 8012H
-MOV B, A
-LDA 8014H
-ADC B
-STA 8018H
-MVI A, 00H
-ACI 00H
-STA 8017H
+JNC BRANCH_ADD
+INR C
 
-RET
+BRANCH_ADD:
+	STA 8004H
+	MOV A, C
+	STA 8005H
+RST 5
 
 ; ------------- Sub -----------------
 SUB:
 
-LDA 8015H
-CMA 
-ADI 01H
-MOV B, A
-LDA 8014H
+MVI C, 00H
+MOV A, C
+STA 8005H
+LDA 8003H
+SUB B
+JNC BRANCH_SUB
 CMA
-ACI 00H
-STA 8014H
-MOV A, B
-STA 8015H
-CALL ADD
-MVI A, 00H
-STA 8017H
+INR A
+INR C
 
-RET
+BRANCH_SUB:
+	STA 8004H
+	MOV A, C
+	STA 8005H
+RST 5
 
 ; ------------- Mul -----------------
 MUL:
 
-; --- STORE E ---
-LDA 8002H
-STA 800AH
+MVI D,00
+MVI A,00
 LDA 8003H
-STA 800BH
+MOV E, A
+MVI A,00
 
-;--------- Start ------
-MVI A, 00H
-STA 8006H
-STA 8007H
-STA 8008H
-STA 8009H
-STA 8016H
-STA 8017H
-STA 8018H
-STA 8019H
-
-LOOP:
-    ;---- COPY B ---  
-    LDA 8004H
-    STA 8014H
-    LDA 8005H
-    STA 8015H
-
-    ;---- COPY TEMP SUM ---
-    LDA 8008H
-    STA 8012H
-    LDA 8009H
-    STA 8013H
-
-    ; ---- ADD B + TEMP SUM ---
-    CALL ADD
-
-    ; --- WRITE BACK TEMP SUM ---
-    LDA 8018H
-    STA 8008H
-    LDA 8019H
-    STA 8009H
-    
-    ; ---- CHECK CARRY ----   
-    LDA 8017H
-    CPI 00H
-
-    ; ---- JUMP ON NO CARRY ---
-    JZ NEXT
-
-    ; ---- IF CARRY, INCREMENT 8006-07 ---
-    LDA 8006H
-    STA 8012H
-    LDA 8007H
-    STA 8013H
-    MVI A, 00H
-    STA 8014H
-    MVI A, 01H
-    STA 8015H
-    CALL ADD 
-
-    ; --- WRITE BACK ----
-    LDA 8018H
-    STA 8006H
-    LDA 8019H
-    STA 8007H   
-
-    MVI C, 11H
-    
-NEXT:
-    ; --- DECREMENT E ----
-    LDA 800AH
-    STA 8012H
-    LDA 800BH
-    STA 8013H
-    MVI A, 00H
-    STA 8014H
-    MVI A, 01H
-    STA 8015H
-
-    CALL SUB
-
-    ; ---- STORE DECREMENTED RESULT ---
-    LDA 8018H
-    STA 800AH
-    LDA 8019H
-    STA 800BH
-
-    ; ---- CHECK IF ZERO ---
-    LDA 800AH
-    MOV B, A
-    LDA 800BH
-    ORA B
-    CPI 00H
-    JNZ LOOP
-
-RET
+LOOP: 
+      ADD B
+	  JNC NEXT
+      INR D
+NEXT: 
+      DCR E
+      JNZ LOOP
+      STA 8005H
+	  MOV A, D
+	  STA 8004H
+RST 5
 
 ; ------------- Div -----------------
 DIV:
 
-LHLD 8002H                      ; Get the dividend
-LDA 8005H                       ; Get the divisor
-MOV C, A
-LXI D, 0000H                    ; Quotient = 0
-
-BACK:   MOV A, H
-        SUB C                   ; Subtract divisor
-        MOV H, A                ; Save partial result
-        JNC SKIP                ; if CY  1 jump
-        DCR L                   ; Subtract borrow of previous subtraction
-
-SKIP:   INX D                   ; Increment quotient
-        MOV A, L
-        CPI 00                  ; Check if dividend < divisor
-        JNZ BACK                ; if no repeat
-
-AGAIN:  MOV A, H
-        SUB C
-        JC END
-        INX D
-        MOV H, A
-        JMP AGAIN
-
-END:    SHLD 8008H              ; Store the remainder
-        XCHG
-        MOV A, H
-        STA  8006H
-        MOV A, L
-        STA 8007H               ; Store the quotient
-                                
-RET                             ; Return back
-
-; ------------- Copy Input ------------
-COPYIN:
-
-LDA 8002H
-STA 8012H
+MVI  C,00
 LDA 8003H
-STA 8013H
-LDA 8004H
-STA 8014H
-LDA 8005H
-STA 8015H
-RET
-
-; ------------- Copy Output -----------
-COPYOUT:
-
-LDA 8016H
-STA 8006H
-LDA 8017H
-STA 8007H
-LDA 8018H
-STA 8008H
-LDA 8019H
-STA 8009H
-RET
+NEXT_2:   
+        CMP B
+        JC  LOOP_2
+        SUB  B
+        INR C
+        JMP NEXT_2
+LOOP_2: 
+        STA 8004H
+        MOV A,C
+        STA 8005H
+RST 5
 
 ; ------------- End -----------------
