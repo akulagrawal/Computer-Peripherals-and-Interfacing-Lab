1ecc9e84e906182e9d97a3f8014574e54513f5b9
Akul Agrawal <akulagrawal@rediffmail.com>
2018-11-12 01:05:59 +0530

    Features added to website

--- Project Kisaan/main/urls.py
+++ Project Kisaan/main/urls.py
@@ -5,5 +5,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$',views.index_view,name='index_main'),
     url(r'search/',views.search_view,name='search_view'),
-    url(r'list/',views.list_view,name='list_view')
+    url(r'list/',views.list_view,name='list_view'),
+    url(r'^search-form/$', views.search_form),
+    url(r'^my/$', views.search)
 ]
--- Project Kisaan/main/views.py
+++ Project Kisaan/main/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 import json
+from .forms import NameForm
 # Create your views here.
 def index_view(request):
     return render(request,'main/search_box.html')
@@ -33,7 +35,50 @@ def search_view(request):
 
 def list_view(request):
 
-	with open('data.json', 'r') as f:
-		data = json.load(f)
+    with open('data.json', 'r') as f:
+        data = json.load(f)
 
-	return render(request,'main/list.html',{'score_data':data})
+    return render(request,'main/list.html',{'score_data':data})
+
+def search_form(request):
+    return render(request, 'main/search_form.html')
+
+def search(request):
+    mode = request.GET['mode']
+    x = request.GET['x']
+    y = request.GET['y']
+    z = request.GET['z']
+    while len(x) < 4:
+        x = "0" + x
+    while len(y) < 4:
+        y = "0" + y
+    while len(z) < 4:
+        z = "0" + z
+
+    with open('coords.txt', 'w') as f:
+        f.write(mode + "," + x + "," + y + "," + z)
+
+    with open('score.json', 'r') as f:
+        score = json.load(f)
+
+    with open('data.json', 'r') as f:
+        data = json.load(f)
+
+    x_step = 500
+    x_max = 3000
+    y_step = 300
+    y_max = 1500
+    x_n = int(x_max/x_step)
+    y_n = int(y_max/y_step)
+    x = score[0]['x']/x_step
+    y = score[0]['y']/y_step
+    pos = int(x*y_n+y)
+    print("Fetching data at position", pos ,"...")
+    #print(data[pos])
+    data[pos] = score[0]
+    #print(data[pos])
+
+    with open('data.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+    return render(request,'main/result.html',{'score_data':score})
